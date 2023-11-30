b51d02d1b9910ee8e03dd0a7af12b5ce313b634d
micorayen <micorayenaniga@gmail.com>
2023-11-30 11:41:37 +0800

    Commit: Add scoring functionality for home and guest
    - Implement the ability to add and deduct scores for both home and guest teams
    
    Markup Changes:
    - Add HTML elements for home and guest score displays
    - Include buttons for adding and deducting scores for both teams
    - Include proper comments Markup structure
    
    JavaScript Changes:
    - Define variables for home and guest score elements
    - Add event listeners for the buttons to handle score changes
    - Include proper comments explaining the purpose and structure of the JavaScript code

--- app.js
+++ app.js
@@ -1,25 +1,74 @@
+// Home and Guest Team Score Elements
+const p1 = {
+  score: 0,
+  buttonAdd: document.getElementById("player1AddScoreButton"),
+  buttonDeduct: document.getElementById("player1DeductScoreButton"),
+  display: document.getElementById("player1ScoreDisplay"),
+};
+
+const p2 = {
+  score: 0,
+  buttonAdd: document.getElementById("player2AddScoreButton"),
+  buttonDeduct: document.getElementById("player2DeductScoreButton"),
+  display: document.getElementById("player2ScoreDisplay"),
+};
+
+let winningScore = 10;
+let isGameOver = false;
+
+// Function for Adding & Deducting Scores
+function updateScore(player, opponent) {
+  if (!isGameOver) {
+    player.score += 1;
+    player.display.textContent = player.score;
+  }
+}
+
+function deductScore(player, opponent) {
+  if (!isGameOver) {
+    player.score -= 1;
+    player.display.textContent = player.score;
+  }
+}
+
+// Event listeners for Home & Guest Team Score Buttons
+p1.buttonAdd.addEventListener("click", () => {
+  updateScore(p1, p2);
+});
+
+p2.buttonAdd.addEventListener("click", () => {
+  updateScore(p2, p1);
+});
+
+p1.buttonDeduct.addEventListener("click", () => {
+  deductScore(p1, p2);
+});
+
+p2.buttonDeduct.addEventListener("click", () => {
+  deductScore(p2, p1);
+});
+
 // Feature: Clock ====================
+const btn_minsToAdd = document.getElementById("mins_to_add");
+const btn_minsToDeduct = document.getElementById("mins_to_deduct");
+const btn_secsToAdd = document.getElementById("secs_to_add");
+const btn_secsToDeduct = document.getElementById("secs_to_deduct");
 
 const btnStart = document.getElementById("start");
 const btnPause = document.getElementById("stop");
+
 // Parameters
-const count = 65;
-const display = document.querySelector("#time");
+let count = 65;
+let display = document.querySelector("#time");
 
 let timer = count;
 let minutes;
 let seconds;
+let intervalId;
 
 function startTimer(duration, display) {
-  let intervalId = setInterval(function () {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
-
-    //=== Display: Timer:
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    display.textContent = minutes + ":" + seconds;
+  intervalId = setInterval(function () {
+    updateDisplay();
 
     //=== Timer: Countdown
     if (--timer < 0) {
@@ -33,13 +82,53 @@ function startTimer(duration, display) {
   btnPause.addEventListener("click", function () {
     clearInterval(intervalId);
     btnStart.disabled = false;
+    btn_minsToAdd.disabled = false;
+    btn_minsToDeduct.disabled = false;
+    btn_secsToAdd.disabled = false;
+    btn_secsToDeduct.disabled = false;
   });
+}
+
+//=== Display: Timer
+function updateDisplay(minutes, seconds) {
+  minutes = parseInt(timer / 60, 10);
+  seconds = parseInt(timer % 60, 10);
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  display.textContent = minutes + ":" + seconds;
 }
 
 //=== Run/Resume Timer
 window.onload = function () {
   btnStart.addEventListener("click", function () {
-    startTimer(count, display);
+    console.log(`Current: ${timer}`);
+    startTimer(timer, display);
     btnStart.disabled = true;
+    btn_minsToAdd.disabled = true;
+    btn_minsToDeduct.disabled = true;
+    btn_secsToAdd.disabled = true;
+    btn_secsToDeduct.disabled = true;
   });
 };
+
+//=== Add/Deduct Mins & Secs to Clock
+btn_minsToAdd.addEventListener("click", function () {
+  timer += 60;
+  updateDisplay();
+});
+
+btn_minsToDeduct.addEventListener("click", function () {
+  timer -= 60;
+  updateDisplay();
+});
+
+btn_secsToAdd.addEventListener("click", function () {
+  timer += 1;
+  updateDisplay();
+});
+
+btn_secsToDeduct.addEventListener("click", function () {
+  timer -= 1;
+  updateDisplay();
+});
